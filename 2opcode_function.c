ba5649760d7ca410dfb0e3eddffd48307037e4d3
Andres Diaz <andresdiaz5010@icloud.com>
2021-09-24 03:53:53 +0000

    New operatos

---
+++ 2opcode_function.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * nop - a
+ * @data_structure: a
+ * @line_number: a
+ *
+ * Return: a
+ */
+void nop(stack_t **data_structure, unsigned int line_number)
+{
+	(void)line_number;
+	(void)data_structure;
+}
